5dc33e6e2f76290049582dbc22d65766eda208fa
Jikrou <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@github.com/Jikrou/zero_day.git>
2023-04-25 19:10:09 -0700

    task

--- functions.c
+++ functions.c
@@ -96,13 +96,35 @@ int print_binary(unsigned int a)
 	return (count);
 }
 
+/**
+ * print_case - prints the argument of the type given
+ * @t: the type of argument to be printed
+ * @args: the argument to print
+ * Return: the number of characters printed
+*/
+int print_case(char t, va_list args)
+{
+	char *ss;
 
-
-
-
-
-
-
-
-
-
+	switch (t)
+	{
+		case'c':
+			return (_putchar(va_arg(args, int)));
+		case's':
+			{
+			ss = va_arg(args, char *);
+			if (ss == NULL)
+				return (_puts("(null)"));
+			return (_puts(ss));
+			}
+		case'd':
+		case'i':
+			return (print_int(va_arg(args, int)));
+		case'b':
+			return (print_binary(va_arg(args, unsigned int)));
+		case'%':
+			return (_putchar('%'));
+		default:
+			return (0);
+	}
+}
